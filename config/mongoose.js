159eee152e9b217210ed25225382b79cd45b271b
ankit <ankit.patidar@daffodilsw.com>
2021-03-07 11:32:36 +0530

    updates

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongo = require("mongoose");
+const baseURL = "mongodb://localhost:27017/mydb";
+mongo
+  .connect(process.env.MONGODB_URI || baseURL, {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  })
+  .then((db) => {
+    console.log("Connected MONGODB");
+  })
+  .catch((error) => console.log("Not Connected"));
